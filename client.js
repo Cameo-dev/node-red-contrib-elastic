86b4d5ba859c499a2c13029acdade1c225efe99b
Jean-Baptiste Richardet <jb@cameo.fr>
2020-05-19 17:36:36 +0200

    Adds JSONata support

--- client.js
+++ client.js
@@ -1,27 +1,30 @@
 const { Client } = require('@elastic/elasticsearch');
 
-module.exports = function (RED) {
+module.exports = function client(RED) {
+  function serverClientNode(config) {
+    RED.nodes.createNode(this, config);
 
-    function serverClientNode(config) {
-        try{throw 'e'} catch(e){}
-        RED.nodes.createNode(this, config);
+    if (!config.cloudId) throw new Error('You must provide a cloud ID');
+    if (!config.username) throw new Error('You must provide a username');
+    if (!this.credentials.password) throw new Error('You must provide a password');
 
-        if(!config.cloudId) throw new Error('You must provide a cloud ID');
-        if(!config.username) throw new Error('You must provide a username');
-        if(!config.password) throw new Error('You must provide a password');
+    const { cloudId: id, username } = config;
+    const { password } = this.credentials;
 
-        const {cloudId: id, username, password} = config;
+    this.client = new Client({
+      cloud: {
+        id,
+      },
+      auth: {
+        username,
+        password,
+      },
+    });
+  }
 
-        this.client = new Client({
-            cloud: {
-                id
-            },
-            auth: {
-                username,
-                password
-            }
-        });
-    }
-
-    RED.nodes.registerType("elasticsearch-client", serverClientNode, {});
-}
+  RED.nodes.registerType('elasticsearch-client', serverClientNode, {
+    credentials: {
+      password: { type: 'password' },
+    },
+  });
+};
--- write.js
+++ write.js
@@ -1,13 +1,19 @@
-/* eslint-disable linebreak-style */
-module.exports = function (RED) {
+const util = require('util');
+
+module.exports = function write(RED) {
   function ElasticsearchWrite(config) {
+    const evaluateJSONataExpression = util.promisify(RED.util.evaluateJSONataExpression);
+
     RED.nodes.createNode(this, config);
 
     if (!config.client) throw new Error('You must provide a client');
-    this.client = RED.nodes.getNode(config.client).client;
+    const clientNode = RED.nodes.getNode(config.client);
+    if (!clientNode) throw new Error('Invalid client node');
+    this.client = clientNode.client;
 
     this.on('input', async (msg, send, done) => {
-      const appendedFields = config.append.reduce((memo, a) => {
+      const appendedFields = await config.append.reduce(async (promisedMemo, a) => {
+        const memo = await promisedMemo;
         switch (a.type) {
           case 'str': memo[a.field] = a.value;
             break;
@@ -15,10 +21,16 @@ module.exports = function (RED) {
             break;
           case 'env': memo[a.field] = process.env[a.value];
             break;
+          case 'jsonata':
+            memo[a.field] = await evaluateJSONataExpression(
+              RED.util.prepareJSONataExpression(a.value, msg),
+              msg,
+            );
+            break;
           default: throw new Error(`Unsupported type ${a.type}`);
         }
         return memo;
-      }, {});
+      }, Promise.resolve({}));
       const index = msg.payload.index || config.index;
       let data;
       try {
